537829f640ffb85104f8d0f15b0b51244d3da38b
swag420yolo <benjsmith314@gmail.com>
2017-01-28 14:33:07 -0600

    Fixed! Sample Autonomous Commands

--- src/Commands/Autonomous.cpp
+++ src/Commands/Autonomous.cpp
@@ -9,6 +9,8 @@ Autonomous::Autonomous(int selection) : CommandGroup("Autonomous") {
 	switch(selection){
 	case 1: //right
 		//forward, turn right, aim, forward
+		AddSequential(new Move(0.5, 1.0));
+		AddSequential(new Rotate(-90));
 		AddSequential(new RadialDrive(0.0));
 		AddSequential(new Move(0.5, 1.0));
 		break;
@@ -19,7 +21,7 @@ Autonomous::Autonomous(int selection) : CommandGroup("Autonomous") {
 	case 3: //left
 		//forward, turn left, aim, forward
 		AddSequential(new Move(0.5, 1.0));
-		AddSequential(new Rotate(-45));
+		AddSequential(new Rotate(45));
 		AddSequential(new RadialDrive(0.0));
 		AddSequential(new Move(0.5, 1.0));
 		break;
